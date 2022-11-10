92e8a8a3f67a2f8313a3780a551f0252a6836502
ifty17 <iftyrahatkabir@gmail.com>
2022-11-10 20:44:42 +0600

    added review

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const servicesCollection = client.db("wildPhotoGraphy").collection("services");
-    const storeReview = client.db("wildPhotoGraphy").collection("storeReview")
+    const servicesCollection = client
+      .db("wildPhotoGraphy")
+      .collection("services");
+    const storeReview = client.db("wildPhotoGraphy").collection("storeReview");
     app.get("/homeServices", async (req, res) => {
       const query = {};
       const cursor = servicesCollection.find(query);
@@ -35,11 +37,11 @@ async function run() {
       res.send(services);
     });
 
-    app.post('/services', async(req, res) =>{
+    app.post("/services", async (req, res) => {
       const add = req.body;
       const result = await servicesCollection.insertOne(add);
       res.send(result);
-    })
+    });
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
@@ -49,36 +51,55 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/storeReview", async(req, res) =>{
+    app.post("/storeReview", async (req, res) => {
       const rev = req.body;
       const result = await storeReview.insertOne(rev);
       res.send(result);
     });
 
     app.get("/reviews", async (req, res) => {
-      const query = {};
-      const cursor = storeReview.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      let query = {};
+      if (req.query.serviceId) {
+        query = {
+          serviceId: req.query.serviceId,
+        };
+      }
+      const result = storeReview.find(query);
+      const reviews = await result.toArray();
+      res.send(reviews);
     });
 
-    app.get("/storeReview", async (req, res) =>{
-      const id = req.query.serviceId;
-      const query = {};
-      const cursor = storeReview.find(query);
-      const result = await cursor.toArray();
-      const serviceId = result.filter(review => review.serviceId === id);
-      res.send(serviceId);
-    })
+    app.get("/storeReview", async (req, res) => {
+      console.log(req.query.serviceId);
+      let query = {};
+      if (req.query.serviceId) {
+        query = {
+          serviceId: req.query.serviceId,
+        };
+      }
+      const result = storeReview.find(query);
+      const reviews = await result.toArray();
+      res.send(reviews);
+    });
 
+    // app.get("/reviewsbyid", async (req, res) => {
+    //   let query = {};
+    //   if (req.query.serviceId) {
+    //     query = {
+    //       serviceId: req.query.serviceId,.sort({ dateField: -1 })
+    //     };
+    //   }
+    //   const result = reviewCollection.find(query).sort({ dateField: -1 });
+    //   const reviews = await result.toArray();
+    //   res.send(reviews);
+    // });
 
-    app.delete('/reviews/:id', async(req, res) =>{
+    app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) };
       const result = await storeReview.deleteOne(query);
       res.send(result);
-    })
-
+    });
   } finally {
   }
 }
