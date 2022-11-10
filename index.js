d178dada0f757bb99f61d4ddf3bdf2d9d550ce56
ifty17 <iftyrahatkabir@gmail.com>
2022-11-10 10:06:32 +0600

    added my services

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ async function run() {
       res.send(services);
     });
 
+    app.post('/services', async(req, res) =>{
+      const add = req.body;
+      const result = await storeReview.insertOne(add);
+      res.send(result);
+    })
+
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
